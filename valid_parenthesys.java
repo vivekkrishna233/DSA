3a162bb79e24514ee3046ba302daf42f57143982
vivek krishna <vivekkrishna233@gmail.com>
2022-06-12 01:41:14 +0530

    commit

---
+++ valid_parenthesys.java
@@ -0,0 +1,24 @@
+public class valid_parenthesys {
+    public boolean isValid(String s) {
+        Stack<Character>st=new Stack<Character>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('||ch=='['||ch=='{'){
+                st.push(ch);
+            }
+            else if(st.isEmpty()==true){
+                return false;
+            }
+            else if(ch==')' && st.pop()!='(') {
+                return false;
+            }
+            else if(ch==']' && st.pop()!= '['){
+                return false;
+            }
+            else if(ch=='}' && st.pop()!= '{'){
+                return false;
+            }   
+        }    
+        return st.isEmpty();
+    }
+}
